71b20551f443ac1825b14f94745617ab18206f5c
Cindy <yw313@nyu.edu>
2017-11-03 16:32:55 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -39,13 +39,31 @@ sheets = pd.read_excel(data_file_name, sheetname=None, skiprows=0, header=1, na_
 #     print(sh_name)
 
 df = sheets['Plate 1']
+
+#Remove white space and new line at the start and end of column names
+df.columns = df.columns.str.strip()
+
 #Map the function parse_sid to the column 'Sample ID' and \
 #zip it into 3 new columns: PID, Visit and Dilution
 df['PID'], df['Visit'], df['Dilution'] = zip(*df['Sample ID'].map(parse_sid))
 
 #Re-arrange the columns that PID, Visit and Dilution are the first 3 columns
 #The original Sample ID column is removed
-cols = list(df.keys()) #Get all column names as list
+cols = list(df.columns) #Get all column names as list
 cols = cols[-3:] + cols[1:-3] #Re-arrange column names that it start with PID, Visit and Dilution, cols[0] dropped
 
-df = df[cols] #
+#Fill the missing values in column Hospital, Age and Gender
+
+#Get the first lines that has hospital, age and gender info for each PID
+first_lines = df.loc[df.Hospital.notnull(), ['Age', 'Gender']]
+
+#Replace empty age and gender as string 'NA'
+df.loc[df.Hospital.notnull(), ['Age', 'Gender']] = first_lines.replace(np.nan, 'NA')
+
+#Get the columns that need to filled with implied values
+patient_info = df[['Hospital', 'Age', 'Gender']]
+
+#Fill empty cells with previous row
+df.loc[:, ['Hospital', 'Age', 'Gender']] = patient_info.fillna(method='ffill')
+
+#Draw plots codes below
