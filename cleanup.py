324a3a81330e69a11eddfc22b48659c3543fb50a
Cindy <yw313@nyu.edu>
2017-11-05 10:23:46 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -2,6 +2,7 @@ import pandas as pd
 import re
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 # Excel data file name
 data_file_name = '06222016 Staph Array Data.xlsx'
@@ -22,17 +23,22 @@ def parse_sid(sample_id):
     # Loop through the rest of words to get Visit and Dilution info
     if len(words) > 0:
         for w in words:
-            if re.match('V\d', w) and visit is None:  # First check if match 'V\d'\
+            if re.match('[Vv]\d', w) and visit is None:  # First check if match 'V\d'\
                 # If visit is already assigned, skip
                 visit = w  # If yes, assign the word as Visit
             elif re.match('[1-9]0+', w) and dilution is None:  # If not match Visit pattern,\
                 #  match it to Dilution pattern of mutiple '0's after a digit
-                dilution = w
+                dilution = int(w)
             else:
                 pid = pid + ' ' + w  # If the word not matching Visit or Dilution, add it to PID
 
     return ([pid, visit, dilution])  # Return the parsed 3 variables as list
 
+# Read in the variables that need to be plotted
+cols_to_plot = []
+with open(layout_file_name, 'r') as fh:
+    for line in fh:
+        cols_to_plot.extend(line.strip().split('\t'))
 
 # Read all sheets in excel file, skip first line, define na as 'NaN'
 sheets = pd.read_excel(data_file_name, sheetname=None, skiprows=0, header=1, na_values='NaN')
@@ -57,12 +63,11 @@ for sheet_name, df in sheets.items():
     df = pd.DataFrame(df[column_names])
 
     # Fill the missing values in column Hospital, Age and Gender
-
     if 'Hospital' in df.columns:
         # Get the lines with hospital value, these are the first row for each patient
         first_lines = df.loc[df.Hospital.notnull(), ['Age', 'Gender']]
 
-        # Replace empty age and gender as string 'NA'
+        # Replace empty age and gender as empty string 'NA'
         df.loc[df.Hospital.notnull(), ['Age', 'Gender']] = first_lines.replace(np.nan, 'NA')
 
         # Get the columns that need to filled with implied values
@@ -71,33 +76,53 @@ for sheet_name, df in sheets.items():
         # Fill empty cells with previous row
         df.loc[:, ['Hospital', 'Age', 'Gender']] = patient_info.fillna(method='ffill')
 
+        # Change string 'NA' to np.nan to keep consistency
+        df = df.replace('NA', np.nan)
+
     # Output formated dataframe to text file
     out_file_name = sheet_name + '.txt'
     df.to_csv(out_file_name, sep='\t', na_rep=np.nan, index=False)
+    print(out_file_name + ' is complete.')
 
 
     # Draw plots codes below
-    # Read in the variables that need to be plotted
-    cols_to_plot = []
-    with open(layout_file_name, 'r') as fh:
-        for line in fh:
-            cols_to_plot.extend(line.strip().split('\t'))
 
-    col = cols_to_plot[0]
+    # Create a directory for all plots of the plate with the name of the plate if not exist
+    plot_dir = sheet_name
+    if not os.path.exists(plot_dir):
+        os.makedirs(plot_dir)
 
+    # Split the data frame by patient ID
     pid_groups = df.groupby('PID')
 
-    pid = '33142'
-    pid_data = pid_groups.get_group(pid)
-    gender = list(pid_data['Gender'])[0]
-    age = int(list(pid_data['Age'])[0])
-    hospital = list(pid_data['Hospital'])[0]
+    # A loop to loop through each of the patient ID
+    for pid in pid_groups.groups.keys():  # pid_groups.groups is dictionary with PID as keys
+        # Get the data of specific PID
+        pid_data = pid_groups.get_group(pid)
+        # Set the Dilution as the index for the data frame
+        pid_data.set_index('Dilution', inplace=True)
 
+        # Make first part of plot title string
+        gender = str(list(pid_data['Gender'])[0])
+        age = str(int(list(pid_data['Age'])[0]))
+        hospital = str(list(pid_data['Hospital'])[0])
+        plot_title_pre = '{}({} {} yr {})'.format(pid, gender, age, hospital)
 
-    plot_title = '{}({} {} yr {}) {}'.format(pid, gender, age, hospital, col)
+        # A loop for creating plots for each of the columns need to be plotted
+        for col_to_plot in cols_to_plot:
+            # Set plot file name as sheet_name-PID-col_to_plot
+            plot_fn = '{}/{}-{}-{}.png'.format(plot_dir, sheet_name, pid, col_to_plot)
 
-    # pid_data.groupby('Visit')[['Dilution','surface protein ext']].plot(marker='o', linestyle='-')
-    pid_data.set_index('Dilution', inplace=True)
-    pid_data.groupby('Visit')['surface protein ext'].plot(legend=True, marker='o', markerfacecolor='white')
-    # pd.factorize(pid_data.Dilution)
-    plt.plot(pid_data.groupby('Visit')['surface protein ext'])
+            # Add the analyte name to the end of plot title
+            plot_title = plot_title_pre + ' ' + col_to_plot
+
+            pid_data.groupby('Visit')[col_to_plot].plot(marker='o', markerfacecolor='white')
+            plt.xlim(min(pid_data.index)/2, max(pid_data.index)*2)
+            plt.xscale('log')
+            plt.yscale('log')
+            plt.legend(title='Visit')
+            plt.ylabel('Intensity')
+            plt.title(plot_title)
+            plt.savefig(plot_fn)
+            plt.close()
+
